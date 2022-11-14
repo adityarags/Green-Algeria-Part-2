4941ac93dafc190b11f2e425da513b63301e7461
R. Aditya <59119736+adityarags@users.noreply.github.com>
2022-11-14 18:06:44 +0530

    Update ModelDeployment.py

--- ModelDeployment.py
+++ ModelDeployment.py
@@ -4,14 +4,13 @@ import numpy as np
 model = pickle.load(open("rf_model.h5", "rb"))
 
 
-TEXT = "Pressed"
 croplist = ["Wheat", "Ground Nuts", "Garden flowers", "Maize", "Paddy", "Potato","Pulse", "Suger Cane", "Coffee"]
-st.title("Water Recommendation System")
+st.title("Recommendation System for Water Management")
 crop_type = st.selectbox("Crop Type", croplist)
-crop_days = st.number_input('Number of days from which the crop was sown')
-soil_moisture = st.number_input('Current SoilMoisture')
+crop_days = st.number_input('Number of days passed after the crop was sown')
+soil_moisture = st.number_input('Current Soil Moisture (% [DHT11])')
 temp = st.number_input('Current Temperature (°C)')
-humid = st.number_input('Current Humidity (% [DHT11])')
+humid = st.number_input('Current Humidity (%)')
 submit = st.button("Submit")
 if submit:
     param1 = croplist.index(crop_type) + 1
@@ -24,4 +23,4 @@ if submit:
     if op[0] == 0:
         st.success('Your crop is irrigated!', icon="✅")
     else:
-        st.warning('Your crop needs irrigation', icon="⚠️")
+        st.warning('Your crop needs irrigation!', icon="⚠️")
